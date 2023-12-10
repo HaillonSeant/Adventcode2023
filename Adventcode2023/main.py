2dec9a4a2cc5f9bd80d78c9f9c3e923ddc3f8034
HaillonSeant <153366717+HaillonSeant@users.noreply.github.com>
2023-12-10 13:58:54 +0100

    Add files via upload

---
+++ Adventcode2023/main.py
@@ -0,0 +1,31 @@
+import sys
+import time
+input=open(sys.argv[1],"r")
+lines=input.read().strip()
+lines=lines.split("\n")
+def Creagrille(lines):
+    grille=[]
+    for i in range(len(lines)):
+        grille.append([])
+        for i2 in range(len(lines[i])):
+            grille[i].append(lines[i][i2])       
+def Spos(grille):
+    for y in range(len(grille)):
+        for x in range(len(grille[0])):
+            if grille[y][x]=="S":
+                return(y,x)
+def Start(grille,spos):
+    if grille[spos[0]][spos[1]-1] in "L-F" and spos[1]-1>=0:
+        return (spos[0],spos[1]-1)
+    if grille[spos[0]-1][spos[1]] in "F|7" and spos[0]-1>=0:
+        return (spos[0]-1,spos[1])
+    if grille[spos[0]][spos[1]+1] in "J7-":
+        return (spos[0],spos[1]+1)
+    if grille[spos[0]+1][spos[1]] in "|JL":
+        return (spos[0]+1,spos[1])
+def Avancer(npos,opos):
+    if grille[pos[0]][pos[1]] == "J"
+
+
+
+spos=Trouvs(grille)
