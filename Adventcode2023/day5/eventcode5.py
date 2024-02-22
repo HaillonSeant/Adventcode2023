eb54742d82d486ddf64b5a9c481d2536d1cef604
HaillonSeant <153366717+HaillonSeant@users.noreply.github.com>
2024-02-22 14:08:07 +0100

    Update eventcode5.py

--- Adventcode2023/day5/eventcode5.py
+++ Adventcode2023/day5/eventcode5.py
@@ -148,6 +148,7 @@ def Dans_lordre(keys):
 # 			tmp.append(i)
 # 	return tmp
 # def Create_range(liseed):
+#solution test = 42
 liseed=Recupdata(data)
 # soluce=Find_min()
 # print('premiere partie : ',ti()-start)
